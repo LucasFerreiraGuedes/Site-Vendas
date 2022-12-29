53905abd6b78adbb23109eccd3abca4569eb2538
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2022-12-29 19:40:48 -0300

    Criando Projeto e Adição do Carrossel

---
+++ Vendas/Vendas/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vendas.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Vendas/Vendas/Controllers/TesteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vendas.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
