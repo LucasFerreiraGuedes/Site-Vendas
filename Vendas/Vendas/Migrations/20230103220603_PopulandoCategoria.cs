451068692ec6bf5a6d8aeae2bb4d7f7f934d9c53
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2023-01-03 19:27:13 -0300

    Populando as Tabelas utilizando o Migrations

---
+++ Vendas/Vendas/Migrations/20230103220603_PopulandoCategoria.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vendas.Migrations
+{
+    public partial class PopulandoCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO CATEGORIAS (CategoriaNome,Descricao) Values ('XBOX ONE','Jogos para a Plataforma Xbox One console criado pela Microsoft')");
+            migrationBuilder.Sql("INSERT INTO CATEGORIAS (CategoriaNome,Descricao) Values ('PS4','Jogos para a Plataforma Playstation, console criado pela Sony')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM CATEGORIAS"); 
+        }
+    }
+}
---
+++ Vendas/Vendas/Migrations/20230103221107_PopulandoJogos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vendas.Migrations
+{
+    public partial class PopulandoJogos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Jogos (Nome,DescricaoCurta,DescricaoDetalhada,Preco,JogoPreferido,EmEstoque,CategoriaId) " +
+                "VALUES ('Mortal Kombat 21','Mortal Kombat 11 é um jogo eletrônico de combate em que dois jogadores lutam um contra o outro ou contra uma inteligência artificia.','Mortal Kombat 11 é um jogo eletrônico de combate em que dois jogadores lutam um contra o outro ou contra uma inteligência .',199.99,0,'1',1)");
+            migrationBuilder.Sql("INSERT INTO Jogos (Nome,DescricaoCurta,DescricaoDetalhada,Preco,JogoPreferido,EmEstoque,CategoriaId) " +
+                "VALUES ('Mortal Kombat 21','Mortal Kombat 11 é um jogo eletrônico de combate em que dois jogadores lutam um contra o outro ou contra uma inteligência artificial nu','Mortal Kombat 11 é um jogo eletrônico de combate em que dois jogadores lutam um contra o outro ou contra uma inteligência artificial.',199.99,0,'1',2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Jogos");
+        }
+    }
+}
